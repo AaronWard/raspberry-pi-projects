3f98d3acc3d81f7f7e30cd5be9ffb766b80f112a
AaronWard <you@example.com>
2020-10-06 12:50:07 +0100

    ADD - printing of DC power

--- freenove/Code/Python_Code/04.1.1_BreathingLED/BreathingLED.py
+++ freenove/Code/Python_Code/04.1.1_BreathingLED/BreathingLED.py
@@ -22,10 +22,12 @@ def setup():
 def loop():
     while True:
         for dc in range(0, 101, 1):   # make the led brighter
+            print(dc)
             p.ChangeDutyCycle(dc)     # set dc value as the duty cycle
             time.sleep(0.01)
         time.sleep(1)
         for dc in range(100, -1, -1): # make the led darker
+            print(dc)
             p.ChangeDutyCycle(dc)     # set dc value as the duty cycle
             time.sleep(0.01)
         time.sleep(1)
